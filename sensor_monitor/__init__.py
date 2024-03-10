a9d12ae023182d5ebf64f88c6b614baca091a940
Aaron <23297034+Aaronsss@users.noreply.github.com>
2024-03-10 17:07:12 +0000

    Updated readme and added drop down with possible sensors in

--- sensor_monitor/__init__.py
+++ sensor_monitor/__init__.py
@@ -2,9 +2,16 @@
 
 import logging
 logger = logging.getLogger(__name__)
+from dataclasses import dataclass, asdict 
 from eventmanager import Evt
 from EventActions import ActionEffect
 from RHUI import UIField, UIFieldType, UIFieldSelectOption
+
+@dataclass
+class SensorInfo():
+    group: str
+    name: str
+    txt: str
 
 class sensor_monitor():
     cellCount = 100
@@ -13,13 +20,13 @@ class sensor_monitor():
     def __init__(self, rhapi):
         self._rhapi = rhapi
 
-    def send_message(self, group, sensor, value, updown, message_types):
+    def send_message(self, sensor, value, updown, message_types):
         if message_types == 0 or message_types == 3:
-            self._rhapi.ui.message_notify(group + " " + sensor[1:] + " is " + updown + " the warning level and is currently: " + str(value))
+            self._rhapi.ui.message_notify(sensor + " is " + updown + " the warning level and is currently: " + str(value))
         if message_types == 1 or message_types == 4:   
-            self._rhapi.ui.message_alert(group + " " + sensor[1:] + " is " + updown + " the alarm level and is currently: " + str(value))
+            self._rhapi.ui.message_alert(sensor + " is " + updown + " the alarm level and is currently: " + str(value))
         if message_types >= 2:
-            self._rhapi.ui.message_speak(group + " " + sensor[1:] + " is currently " + str(value))
+            self._rhapi.ui.message_speak("Alert " + sensor.replace("_", " ") + " is currently " + str(value))
 
     def calculate_cells(self, args, volts):
         if volts > 6 and volts <= 30:
@@ -38,44 +45,43 @@ class sensor_monitor():
             logger.info("[Sensor Monitor] Battery cells not calculated as voltage outside of 6-30V range")
 
     def check_sensors(self, action, args):
+        sensor_info = self.sensor_list[int(action['sensors'])]
         try:
-            Sensor_data = round(float(getattr(self._rhapi.sensors.sensor_obj(action['group']), action['name'])), 2)  
-        #Sensor_data = float(action['group'])
+            Sensor_data = round(float(getattr(self._rhapi.sensors.sensor_obj(sensor_info.group), sensor_info.name)), 2)  
             if action['compare_type'] == '2' or action['compare_type'] == '3': # Use cell count 
                 if self.cellCount == 100:
                     self.calculate_cells(args, Sensor_data)
                 Sensor_data = round(Sensor_data / self.cellCount, 2)
             if action['compare_type'] == '0' or action['compare_type'] == '2': # less than 
                 if Sensor_data < float(action['warn_value']):
-                    self.send_message(action['group'], action['name'], str(Sensor_data), "below", int(action['warn_type']))
+                    self.send_message(sensor_info.txt, str(Sensor_data), "below", int(action['warn_type']))
             elif action['compare_type'] == '1' or action['compare_type'] == '3': # greater than 
                 if Sensor_data > float(action['warn_value']):
-                    self.send_message(action['group'], action['name'], str(Sensor_data), "above", int(action['warn_type']))
+                    self.send_message(sensor_info.txt, str(Sensor_data), "above", int(action['warn_type']))
         except:
-            logger.info("[Sensor Monitor] " + action['group'] + action['name'] + " not recognised!")
+            logger.info("[Sensor Monitor] " + sensor_info.txt + " not recognised!")
 
     def discover_sensors(self, args):
         self.sensor_list.clear()
-        logger.info("[Sensor Monitor] Available groups and sensor")
         for x in self._rhapi.sensors.sensor_names:
-            logger.info('[Sensor Monitor] -> Group name: '+ str(x))
             for y in vars(self._rhapi.sensors.sensor_obj(x)):
                 if y.find("_") == 0:
-                    logger.info("[Sensor Monitor] --> Sensor name: " + str(y) + " " + str(getattr(self._rhapi.sensors.sensor_obj(x), y)))
-                    self.sensor_list.append((str(x) + str(y)))
-        for x in self.sensor_list:
-            print("[Sensor Monitor] " + x) 
+                    self.sensor_list.append(SensorInfo(str(x), str(y), (str(x) + str(y))))
     
     def register_handlers(self, args):
         self.discover_sensors(args)
+        sensor_options = []
+        if not self.sensor_list:
+            sensor_options.append(UIFieldSelectOption(0, "--- No Sensors Detected! ---"))
+        for i, x in enumerate(self.sensor_list):
+            sensor_options.append(UIFieldSelectOption(i, x.txt))
         if 'register_fn' in args:
             for effect in [
                 ActionEffect(
                     'Sensor Monitor',
                     self.check_sensors,
                     [
-                        UIField('group', "Sensor Group", UIFieldType.TEXT),
-                        UIField('name', "Sensor Name", UIFieldType.TEXT),
+                        UIField('sensors', "Sensor", UIFieldType.SELECT, options=sensor_options, value=0),
                         UIField('warn_type', "Warning Type", UIFieldType.SELECT, options=[
                             UIFieldSelectOption(0, "Message Only"),
                             UIFieldSelectOption(1, "Alert Only"),
@@ -98,4 +104,3 @@ class sensor_monitor():
 def initialize(rhapi):
     sensor_mon = sensor_monitor(rhapi)
     rhapi.events.on(Evt.ACTIONS_INITIALIZE, sensor_mon.register_handlers)
-    #rhapi.events.on(Evt.STARTUP, sensor_mon.discover_sensors)
